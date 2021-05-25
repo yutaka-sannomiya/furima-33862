5372d79acbf43f3ac78a2822491527075e016b52
yutaka-sannomiya <yutakasannnomiya@gmail.com>
2021-05-25 19:16:29 +0900

    プルリクエスト修正２回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all
-    #@items = Item.order("created_at DESC")※残しておくように指示がありました。
+    @items = Item.order("created_at DESC")
   end
 
   def new
