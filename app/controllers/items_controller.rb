9752f1f61c5b7e1be675f685fa67b25110acbdf2
yutaka-sannomiya <yutakasannnomiya@gmail.com>
2021-05-27 21:10:24 +0900

    ログアウト状態での挙動確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,7 +20,6 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @user = User.find(params[:id])
   end
 
 
