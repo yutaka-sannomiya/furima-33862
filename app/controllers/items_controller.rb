dbb32dbe318b6ce59e1c519ea4c92e0a245d03cc
yutaka-sannomiya <yutakasannnomiya@gmail.com>
2021-05-29 17:09:00 +0900

    編集画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,11 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(@item_params)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
 
