f1840a19314bf6d536f506f096ddf4398107b79c
yutaka-sannomiya <yutakasannnomiya@gmail.com>
2021-05-29 20:30:19 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
     if @item.destroy
       redirect_to root_path
     else
-      redirect_to root_path
+      redirect_to item_path
     end
   end
 
